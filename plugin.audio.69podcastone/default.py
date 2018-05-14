dddcadb16eb0471d139896ab88314a001150762f
muaddibttv <31803191+muaddibttv@users.noreply.github.com>
2018-05-13 21:27:08 -0400

    ffs lol

--- plugin.audio.69podcastone/default.py
+++ plugin.audio.69podcastone/default.py
@@ -17,7 +17,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import __builtin__
-import xbmcaddon
+import xbmcaddon,xbmcgui
 
 # CONFIGURATION VARIABLES
 # -----------------------
